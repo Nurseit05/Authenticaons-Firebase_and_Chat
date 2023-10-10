38ec76e41f31ed2ca7e0f2ce8787795dbe646fa9
Nurseyit Abdyldaev <nurseyit2005@gmail.com>
2023-10-10 19:14:52 +0600

    ...

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,46 +1,28 @@
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import React, { useContext } from 'react';
 import {useAuthState} from 'react-firebase-hooks/auth'
 
 import {privateRoutes, publicRoutes} from '../routes'
 import { Context } from '../index';
-import Login from './Login';
-import Chat from './Chat';
 
 
 const AppRouter = () => {
-    
     const {auth} = useContext(Context)
     const [user] = useAuthState(auth);
-    const navigate = useNavigate(); // Получаем функцию для навигации
-
-    if (user) {
-      navigate('/chat');
-    } else {
-      navigate('/login');
-    }
-  
-    return (
-      <Routes>
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    );
-
-
-    // return user ? (
-    //     <Routes>
-    //         {privateRoutes.map(({path, element}) => 
-    //             <Route key={path} path={path} element={element} />
-    //         )}
-    //     </Routes>
-    // ) : (
-    //     <Routes>
-    //         {publicRoutes.map(({path, element}) => 
-    //             <Route key={path} path={path} element={element} />
-    //         )}
-    //     </Routes>
-    // )
+    
+    return user ? (
+        <Routes>
+            {privateRoutes.map(({path, element}) => 
+                <Route key={path} path={path} element={element} />
+            )}
+        </Routes>
+    ) : (
+        <Routes>
+            {publicRoutes.map(({path, element}) => 
+                <Route key={path} path={path} element={element} />
+            )}
+        </Routes>
+    )
 };
 
 export default AppRouter;
